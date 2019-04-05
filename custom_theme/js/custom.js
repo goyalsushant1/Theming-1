8e08c0713da2837beaa4ad529e1d033566e0dd15
goyalsushant1 <sushant.goyal@tothenew.com>
2019-04-05 09:23:12 +0530

    Assignment Upload

---
+++ custom_theme/js/custom.js
@@ -0,0 +1,7 @@
+function increaseSize() {
+  document.getElementsByClassName('title')[0].style.fontSize = '40px';
+}
+
+function defaultSize() {
+  document.getElementsByClassName('title')[0].style.fontSize = '24px';
+}
